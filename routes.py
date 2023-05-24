076dd5f92968f9af456a10d9bd7272f8d2aa0434
ToshaniN <toshaninath@gmail.com>
2023-05-24 17:08:38 -0400

    Removed extra slash from post route

--- routes.py
+++ routes.py
@@ -4,4 +4,4 @@ from url_resource import *
 class Routes:
     api.add_resource(Home, '/')
     api.add_resource(WantShortURL, '/WantShortURL/')
-    api.add_resource(WantLongURL, '/WantLongURL/')
+    api.add_resource(WantLongURL, '/WantLongURL')
