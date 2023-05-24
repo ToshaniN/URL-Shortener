6be4251130c4e75deb7d3a2eb478ef1060e822cd
ToshaniN <toshaninath@gmail.com>
2023-05-24 12:34:14 -0400

    Added cors

--- url_resource.py
+++ url_resource.py
@@ -1,6 +1,7 @@
 from handler import Handler
 from flask_restful import Resource
 from flask import request, make_response
+from flask_cors import cross_origin
 
 # '/'
 class Home(Resource):
@@ -15,6 +16,14 @@ class WantShortURL(Resource):
 
 # '/WantLongURL/'
 class WantLongURL(Resource):
+    # def options(self):
+    #     response = make_response()
+    #     response.headers.add("Access-Control-Allow-Origin", "*")
+    #     response.headers.add('Access-Control-Allow-Headers', "*")
+    #     response.headers.add('Access-Control-Allow-Methods', "*")
+    #     response.headers.add('Content-Type', 'application/json')
+    #     return response
+    
     def post(self):
         receivedShortURL = request.get_json()
         shortURL = receivedShortURL["shortURL"]
